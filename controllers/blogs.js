3278e70afd313cfeb6b0a7830557c37b164ab5ba
therealartti <gersterarthur@icloud.com>
2023-09-12 16:09:34 +0300

    13.5 + 13.6

---
+++ controllers/blogs.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+})
+  
+router.post('/', async (req, res) => {
+try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+} catch(error) {
+    res.status(400).json({ error })
+}
+})
+
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+  
+router.delete('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        await req.blog.destroy()
+    }
+    res.status(204).end()
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+      req.blog.likes = req.body.likes
+      await req.blog.save()
+      res.json({likes: req.blog.likes})
+    } else {
+      res.status(404).end()
+    }
+})
+
+module.exports = router
