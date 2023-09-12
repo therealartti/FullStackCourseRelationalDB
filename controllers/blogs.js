4dcdafc022ba3df6a70078fcd12c7df9dff838ea
therealartti <gersterarthur@icloud.com>
2023-09-12 16:34:27 +0300

    13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,12 +7,12 @@ router.get('/', async (req, res) => {
     res.json(blogs)
 })
   
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
 try {
     const blog = await Blog.create(req.body)
     res.json(blog)
 } catch(error) {
-    res.status(400).json({ error })
+    next(error)
 }
 })
 
@@ -28,14 +28,23 @@ router.delete('/:id', blogFinder, async (req, res) => {
     res.status(204).end()
 });
 
-router.put('/:id', blogFinder, async (req, res) => {
+router.put('/:id', blogFinder, async (req, res, next) => {
+    try {
     if (req.blog) {
       req.blog.likes = req.body.likes
       await req.blog.save()
       res.json({likes: req.blog.likes})
     } else {
       res.status(404).end()
+    }} catch(error){
+        next(error)
     }
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    return response.status(500).json({ error: error.message })
+}
+router.use(errorHandler)
 
 module.exports = router
