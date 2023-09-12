8e93d92b5386a858773607337eb1ccc4cda04405
therealartti <gersterarthur@icloud.com>
2023-09-12 13:39:49 +0300

    13.4

--- cli.js
+++ cli.js
@@ -1,20 +1,64 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-    try {
-      await sequelize.authenticate()
-      const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-      for (let blog of blogs) {
-        const { author, title, likes } = blog
-        console.log(`${author}: '${title}', ${likes} likes`)
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await Blog.destroy({ where: { id } })
+    if (result === 0) {
+      return res.status(404).json({ error: 'Blog not found' })
     }
-      sequelize.close()
-    } catch (error) {
-      console.error('Unable to connect to the database:', error)
-    }
-}
-  
-main()
+    return res.status(204).end()
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
