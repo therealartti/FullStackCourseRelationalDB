d607e01ac439faca69a5b40c4ba8ed784a025631
therealartti <gersterarthur@icloud.com>
2023-09-13 16:09:30 +0300

    13.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,9 +10,10 @@ router.get('/', async (req, res) => {
     const where = {}
 
     if (req.query.search) {
-        where.title = {
-            [Op.iLike]: `%${req.query.search}%`
-        }
+      where[Op.or] = [
+        { title: { [Op.iLike]: `%${req.query.search}%`}},
+        { author: { [Op.iLike]: `%${req.query.search}%` }}
+      ]
     }
 
     const blogs = await Blog.findAll({
