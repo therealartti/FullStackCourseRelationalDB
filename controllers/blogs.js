4b7799eeb12ba2b99ddd236374d833177d39598b
therealartti <gersterarthur@icloud.com>
2023-09-13 14:03:11 +0300

    13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,33 @@
+const jwt = require('jsonwebtoken')
 const router = require('express').Router()
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+const { SECRET } = require('../util/config')
+
 
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
 })
+
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      try {
+        req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      } catch{
+        return res.status(401).json({ error: 'token invalid' })
+      }
+    }  else {
+      return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+}
   
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
 try {
-    const blog = await Blog.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({...req.body, userId: user.id, date: new Date()})
     res.json(blog)
 } catch(error) {
     next(error)
