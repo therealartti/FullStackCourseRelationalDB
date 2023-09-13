57e147bf321ac8afe684ef7ff8c48645c070a3aa
therealartti <gersterarthur@icloud.com>
2023-09-13 12:55:03 +0300

    13.8

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({ where: { username: req.params.username } })
+  if (user) {
+    if (req.body.username) {
+        user.username = req.body.username
+        await user.save()
+        res.json({ user })
+    } else {
+        res.status(400).json({ error: 'Username cannot be null or empty' })
+    }
+    } else {
+      res.status(404).end()
+    }
+})
+
+module.exports = router
